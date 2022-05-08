eb8c302fb0044a70b16da6008313e1f15f5efda2
jamirodevulder <31829657+jamirodevulder@users.noreply.github.com>
2022-05-09 01:54:40 +0200

    Network issuese gefixed + steam connectie
    Je kan nu connecten via steam. ook verschillende netwerk problemen opgelost.

---
+++ Source/Overseas/Test.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class OVERSEAS_API Test
+{
+public:
+	Test();
+	~Test();
+};
